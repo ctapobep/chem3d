2e07e36205a86ad5d9a4d6e9d04365b5a0d12e9a
Stanislav Bashkyrtsev <stanislav.bashkyrtsev@elsci.io>
2023-11-26 04:39:32 +0400

    Ability to normalize vectors

--- src/main/js/la/Vector.js
+++ src/main/js/la/Vector.js
@@ -3,12 +3,17 @@ export default class Vector {
     values;
 
     /**
-     * @param {number} x
-     * @param {number} y
-     * @param {number} z
+     * @param {number} components
      */
-    constructor(x, y, z) {
-        this.values = [x, y, z];
+    constructor(...components) {
+        if(!Array.isArray(components))
+            throw new Error("Not an array of numbers passed!");
+        if(components.length < 1)
+            throw new Error("Zero-dimensioned Vectors aren't supported");
+        for (let i = 0; i < components.length; i++)
+            if(!this.#isNumber(components[i]))
+                throw new Error(`Component #${i} is not a number: ` + JSON.stringify(components));
+        this.values = components;
     }
 
     /**
@@ -16,12 +21,23 @@ export default class Vector {
      * @return {number}
      */
     dot(that) {
-        if(this.dims() !== that.dims())
-            throw new Error(`Can't dot vectors with different dimensions: ${this.dims()} & ${that.dims}`)
+        this.#assertDimensionsEqual(that);
         let result = 0;
         for (let i = 0; i < this.values.length; i++)
             result += this.get(i) * that.get(i);
         return result;
+    }
+
+    /**
+     * @param {number} scalar
+     * @return {Vector}
+     */
+    scaled(scalar) {
+        /** @type {number[]} */
+        const result = [];
+        for (let i = 0; i < this.values.length; i++)
+            result[i] = this.values[i] * scalar;
+        return new Vector(...result);
     }
 
     /** @returns {number} */
@@ -35,10 +51,33 @@ export default class Vector {
     }
 
     /**
+     * @return {Vector}
+     */
+    normalized() {
+        const norm = this.norm();
+        if(norm === 0)
+            throw new Error("Can't normalize a zero vector: " + this);
+        return this.scaled(1/norm);
+    }
+
+    /**
      * @param {number} componentIndex
      * @returns {number}
      */
     get(componentIndex) {
         return this.values[componentIndex];
+    }
+    toString() {
+        return JSON.stringify(this.values);
+    }
+
+    /** @param {Vector} that */
+    #assertDimensionsEqual(that) {
+        if(this.dims() !== that.dims())
+            throw new Error(`Can't dot vectors with different dimensions: ${this.dims()} & ${that.dims}`);
+    }
+
+    #isNumber(value) {
+        return typeof value === "number" && !Number.isNaN(value);
     }
 }
--- src/test/js/la/VectorTest.js
+++ src/test/js/la/VectorTest.js
@@ -5,9 +5,16 @@ describe("Vector", () => {
     it("can dot", () => {
         assert.strictEqual(new Vector(1, 1, 1).dot(new Vector(0, 0, 0)), 0);
         assert.strictEqual(new Vector(1, 1, 1).dot(new Vector(2, 2, 2)), 6);
-    })
+    });
     it("has length", () => {
         assert.strictEqual(new Vector(0, 0, 0).norm(), 0);
         assert.strictEqual(new Vector(1, 1, 1).norm(), Math.sqrt(3));
-    })
+    });
+    it("can return normalized version of itself with length=1", ()=> {
+        assert.deepStrictEqual(new Vector(1, 0, 0).normalized(), new Vector(1, 0, 0));
+        assert.deepEqual(new Vector(1, 0, 1).normalized(), new Vector(1/Math.sqrt(2), 0, 1/Math.sqrt(2)));
+    });
+    it("errs if normalization is invoked for a zero vector", ()=> {
+        assert.throws(() => new Vector(0).normalized());
+    });
 })
