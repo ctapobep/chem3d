013b28006f82e812466d79206baef2a12cae7142
Stanislav Bashkyrtsev <stanislav.bashkyrtsev@elsci.io>
2024-06-22 15:52:55 +0300

    Turn -0 into 0 when doing math with Vector

--- src/main/js/la/Vector.js
+++ src/main/js/la/Vector.js
@@ -4,16 +4,10 @@ export default class Vector {
     values;
 
     /**
-     * @param {number} components
+     * @param {...number} components
      */
     constructor(...components) {
-        if(!Array.isArray(components))
-            throw new Error("Not an array of numbers passed!");
-        if(components.length < 1)
-            throw new Error("Zero-dimensioned Vectors aren't supported");
-        for (let i = 0; i < components.length; i++)
-            if(!this.#isNumber(components[i]))
-                throw new Error(`Component #${i} is not a number: ` + JSON.stringify(components));
+        this.#validate(components);
         this.values = components;
     }
 
@@ -38,15 +32,28 @@ export default class Vector {
         const result = [];
         for (let i = 0; i < this.values.length; i++)
             result[i] = this.values[i] * scalar;
+        this.#cleanUp(result);
         return new Vector(...result);
     }
 
-    /** @param {Vector} that */
+    /**
+     * @param {Vector} that
+     * @return {Vector}
+     */
     add(that) {
         const result = [];
         for (let i = 0; i < this.values.length; i++)
             result.push(this.get(i) + that.get(i));
+        this.#cleanUp(result);
         return new Vector(...result);
+    }
+
+    /**
+     * @param {Vector} that
+     * @return {Vector}
+     */
+    subtr(that) {
+        return this.add(that.scaled(-1));
     }
 
     /** @returns {number} */
@@ -64,9 +71,9 @@ export default class Vector {
      */
     normalized() {
         const norm = this.norm();
-        if(norm === 0)
+        if (norm === 0)
             throw new Error("Can't normalize a zero vector: " + this);
-        return this.scaled(1/norm);
+        return this.scaled(1 / norm);
     }
 
     /**
@@ -76,17 +83,41 @@ export default class Vector {
     get(componentIndex) {
         return this.values[componentIndex];
     }
+
     toString() {
         return JSON.stringify(this.values);
     }
 
     /** @param {Vector} that */
     #assertDimensionsEqual(that) {
-        if(this.dims() !== that.dims())
+        if (this.dims() !== that.dims())
             throw new Error(`Can't dot vectors with different dimensions: ${this.dims()} & ${that.dims}`);
     }
 
     #isNumber(value) {
         return typeof value === "number" && !Number.isNaN(value);
+    }
+
+    /**
+     * @param {number[]} components
+     */
+    #cleanUp(components) {
+        for (let i = 0; i < components.length; i++) {
+            const val = components[i];
+            if(val === -0)
+                components[i] = 0;
+        }
+    }
+    /**
+     * @param {number[]} components
+     */
+    #validate(components) {
+        if (!Array.isArray(components))
+            throw new Error("Not an array of numbers passed!");
+        if (components.length < 1)
+            throw new Error("Zero-dimensioned Vectors aren't supported");
+        for (let i = 0; i < components.length; i++)
+            if (!this.#isNumber(components[i]))
+                throw new Error(`Component #${i} is not a number: ` + JSON.stringify(components));
     }
 }
